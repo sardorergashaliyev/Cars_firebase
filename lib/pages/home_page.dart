ce7f727cd1cc88deca80bb0a2c836e55e013dfa3
Sardor Ergashaliyev <111111544+sardorergashaliyev@users.noreply.github.com>
2023-01-24 15:23:56 +0500

    fixed facebook

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_test/pages/add_number.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_login_facebook/flutter_login_facebook.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -74,6 +75,59 @@ class _MyHomePageState extends State<MyHomePage> {
                         builder: (a) => const Addnumber(),
                       ),
                     );
+                  },
+                ),
+                const SizedBox(height: 20),
+                GestureDetector(
+                  child: Container(
+                      padding: const EdgeInsets.all(12),
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(14),
+                        color: const Color(0x30858C94),
+                      ),
+                      child: Image.network(
+                          'https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/1024px-Facebook_Logo_%282019%29.png')),
+                  onTap: () async {
+                    final fb = FacebookLogin();
+                    final res = await fb.logIn(permissions: [
+                      FacebookPermission.publicProfile,
+                      FacebookPermission.email,
+                    ]);
+                    switch (res.status) {
+                      case FacebookLoginStatus.success:
+                        // Logged in
+
+                        // Send access token to server for validation and auth
+                        final FacebookAccessToken? accessToken =
+                            res.accessToken;
+                        print('Access token: ${accessToken?.token}');
+
+                        // Get profile data
+                        final profile = await fb.getUserProfile();
+                        print(
+                            'Hello, ${profile?.name}! You ID: ${profile?.userId}');
+
+                        // Get user profile image url
+                        final imageUrl =
+                            await fb.getProfileImageUrl(width: 100);
+                        print('Your profile image: $imageUrl');
+
+                        // Get email (since we request email permission)
+                        final email = await fb.getUserEmail();
+                        // But user can decline permission
+                        if (email != null) print('And your email is $email');
+
+                        break;
+                      case FacebookLoginStatus.cancel:
+                        // User cancel log in
+                        break;
+                      case FacebookLoginStatus.error:
+                        // Log in failed
+                        print('Error while log in: ${res.error}');
+                        break;
+                    }
                   },
                 )
               ],
